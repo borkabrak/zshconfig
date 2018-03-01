f883dd96717430fde4b8de7fee7147d2929224af
Jonathan Carter <borkabrak@gmail.com>
2018-03-01 11:28:33 -0600

    New file 'bindkey.zsh' for key bindings

---
+++ include/bindkey.zsh
@@ -0,0 +1,7 @@
+########################################################################################################
+#vim:ft=zsh
+#
+#   ZSH key bindings
+
+# Alt-u goes up a directory and displays contents
+bindkey -s '\eu' '^Ucd ..; ls^M'
