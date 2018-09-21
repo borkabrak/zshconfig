f9568ca5bf968472e095062b4ab4846da7cc426a
Jonathan Carter <borkabrak@gmail.com>
2018-09-21 05:18:35 -0500

    Remove some unused named directories

--- include/named-directories.zsh
+++ include/named-directories.zsh
@@ -2,12 +2,5 @@
 # vim:set ft=zsh:
 ###################
 
-hash -d htdocs=/home/jon/htdocs
-hash -d www=~htdocs
+hash -d www=~/www
 hash -d javascript=/usr/share/javascript
-hash -d torrent=~/downloads/torrent
-hash -d write=~/documents/write
-hash -d music=~/music
-hash -d steam=~/.local/share/Steam
-hash -d steamapps=~steam/steamapps
-hash -d steamgames=~steam/steamapps/common
